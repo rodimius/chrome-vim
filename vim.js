f31ed153326ce785377524c5eadee22e5ae78d64
robert <robert@archdesktop.(none)>
2009-09-02 12:57:05 +0100

    Added esc binding and fixed propagation.

--- vim.js
+++ vim.js
@@ -1,26 +1,34 @@
 // smelly
+var port = chrome.extension.connect({name: "tabs"});
+var scroll_amount = 50;
 function scrollDown() {
-  window.scrollBy(0,50);
+  window.scrollBy(0,scroll_amount);
 }
 function scrollUp() {
-  window.scrollBy(0,-50);
+  window.scrollBy(0,-scroll_amount);
 }
 function scrollLeft() {
-  window.scrollBy(-50,0);
+  window.scrollBy(-scroll_amount,0);
 }
 function scrollRight() {
-  window.scrollBy(50,0);
+  window.scrollBy(scroll_amount,0);
 }
 function inputKey(inKey, keyFunc) {
-  $(document).bind('keydown', {combi:inKey, disableinInput: true}, keyFunc)
+  $(document).bind('keydown', {combi:inKey, disableinInput: true, propagate:false}, keyFunc)
+}
+
+function specialKey(inKey, keyFunc) {
+  $(document).bind('keydown', {combi:inKey, disableinInput: false, propagate:false}, keyFunc)
 }
 
 function deleteTab() {
-  console.log(chrome.csi());
+        port.postMessage({method:"delete"});
 }
 inputKey('j', scrollDown);
 inputKey('k', scrollUp);
 inputKey('h', scrollLeft);
 inputKey('l', scrollRight);
-inputKey('ctrl+o', function () {history.go(-1);});
-inputKey('ctrl+i', function () {history.go(1);false;});
+specialKey('ctrl+o', function () {history.go(-1);return false;});
+specialKey('ctrl+i', function () {history.go(1);return false;});
+inputKey('d', deleteTab);
+specialKey('esc', function () {$(":input").blur();});
